75831d5445828feb0ed195009b90dfd964ef872e
FanciestW <wlin26@yahoo.com>
2015-01-07 21:10:27 -0500

    Modified pop up message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 $(window).load(function(){
-	alert("This website is still under construction, please be patient we will have it fully furnished in no time. Thank you")
+	alert("This website is still under construction,\
+	 please be patient we will have it fully furnished in no time.\
+	 In the mean time feel Thank you!")
 });
