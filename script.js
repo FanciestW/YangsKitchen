c0a6c4eb7ca4a39840c79763b3ff6d61e3a24121
FanciestW <wlin26@yahoo.com>
2015-01-11 01:02:33 -0500

    temporarily disabled pop up message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Pop-up message
+/*
 $(window).load(function(){
 	alert("This website is still under construction,\
 	 please be patient we will have it fully furnished in no time.\
 	 In the mean time feel Thank you!")
 });
+*/
